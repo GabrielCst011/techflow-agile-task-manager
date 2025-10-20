377cce0b24a58350fcd83bfcb5c05f56ff3766ff
Gabriel Costa <bbbgabriel35@outlook.com>
2025-10-20 16:14:03 -0300

    Implement basic CRUD for tasks with Flask and SQLAlchemy

---
+++ src/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, request
+from models import db, Task
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+db.init_app(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    tasks = Task.query.all()
+    return jsonify([t.to_dict() for t in tasks])
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    data = request.get_json()
+    task = Task(title=data['title'], status='To Do')
+    db.session.add(task)
+    db.session.commit()
+    return jsonify(task.to_dict()), 201
+
+@app.route('/tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+    task = Task.query.get(id)
+    data = request.get_json()
+    task.title = data.get('title', task.title)
+    task.status = data.get('status', task.status)
+    db.session.commit()
+    return jsonify(task.to_dict())
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    task = Task.query.get(id)
+    db.session.delete(task)
+    db.session.commit()
+    return '', 204
+
+if __name__ == '__main__':
+    app.run(debug=True)
